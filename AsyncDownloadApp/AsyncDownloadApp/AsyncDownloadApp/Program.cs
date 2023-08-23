642166f68ecacf9b94ce52ec906e26fc1c3ecc48
Katarina Novikova <novikkatarina@gmail.com>
2023-08-23 21:01:18 +0400

    First

---
+++ AsyncDownloadApp/AsyncDownloadApp/AsyncDownloadApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+
+Console.Write("Введите URL-адрес файла: ");
+string url = Console.ReadLine();
+
+Console.Write("Введите путь сохранения файла: ");
+string path = Console.ReadLine();
+
+await AsyncDownloadSave(url, path);
+
+Console.WriteLine("Файл был успешно загружен и сохранен.");
+
+static async Task AsyncDownloadSave(string path,string name)
+{
+  try{
+    using (var client = new WebClient())
+    {
+      client.DownloadFile(path,name);
+    }
+  }
+  catch (Exception e)
+  {
+    Console.WriteLine(e);
+    throw;
+  }
+}
