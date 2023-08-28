ea6cad01046e9c48d2fb962ac4d73af2b53b9fa2
Katarina Novikova <novikkatarina@gmail.com>
2023-08-28 16:41:34 +0400

    Tests in progress2

--- Phonebook-master/Phonebook.Tests/GetAllTest.cs
+++ Phonebook-master/Phonebook.Tests/GetAllTest.cs
@@ -1,12 +1,28 @@
 using NUnit.Framework;
+using Phonebook;
 
 namespace PhonebookTest;
 
 public class GetAllTest
 {
+    private List<PhoneNumber> PhoneNumber { set; get; }
+    // private List<Subscriber> Subscribers { set; get; }
+    private Phonebook.Phonebook Phonebook { set; get; }
+    
     [Test]
     public void GetAll_AllSubscribers_SuccessfullyReceived()
     {
+        //Arrange
+        PhoneNumber phoneNumber = new PhoneNumber("13452", PhoneNumberType.Personal);
+        PhoneNumber.Add(phoneNumber);
+        Subscriber existedSubscriber = new Subscriber(Guid.NewGuid(), "DFR", PhoneNumber);
+        Phonebook.AddSubscriber(existedSubscriber);
         
+        // Act
+
+        Subscriber findedSubscriber = Phonebook.GetSubscriber(existedSubscriber.Id);
+        
+        //Assert
+        Assert.AreEqual(existedSubscriber,findedSubscriber);
     }
 }
--- Phonebook-master/Phonebook.Tests/GetSubscriberTest.cs
+++ Phonebook-master/Phonebook.Tests/GetSubscriberTest.cs
@@ -1,22 +1,26 @@
 ﻿using NUnit.Framework;
-using Phonebook;
-using Phonebook = Phonebook.Phonebook;
 
-namespace PhonebookTest;
+namespace Phonebook.Tests;
+using System.Data.Objects;
 
 public class Tests
 {
     private List<PhoneNumber> PhoneNumber { set; get; }
-    private List<Subscriber> Subscribers { set; get; }
-    private Phonebook.Phonebook Phonebook { set; get; }
-    
+    private Phonebook Phonebook { set; get; }
+
     [SetUp]
     public void Setup()
     {
         Phonebook = new Phonebook();
-       Subscribers = new List<Subscriber>();
-       PhoneNumber = new List<PhoneNumber>();
+        PhoneNumber = new List<PhoneNumber>();
     }
+
+    [TearDown]
+    public void TearDown()
+    {
+        DeleteObject(Phonebook);
+    }
+    
     
     [Test]
     public void GetSubscriber_ExistedSubscriber_SuccessfullyFound()
@@ -24,14 +28,12 @@ public class Tests
         //Arrange
         PhoneNumber phoneNumber = new PhoneNumber("13452", PhoneNumberType.Personal);
         PhoneNumber.Add(phoneNumber);
-        Subscriber existedSubscriber = new Subscriber(Guid.NewGuid(), "DFR", PhoneNumber); 
-        // Phonebook.Phonebook ph = new Phonebook.Phonebook();
-        
-        ph.AddSubscriber(existedSubscriber);
+        Subscriber existedSubscriber = new Subscriber(Guid.NewGuid(), "DFR", PhoneNumber);
+        Phonebook.AddSubscriber(existedSubscriber);
         
         // Act
 
-        Subscriber findedSubscriber = ph.GetSubscriber(existedSubscriber.Id);
+        Subscriber findedSubscriber = Phonebook.GetSubscriber(existedSubscriber.Id);
         
         //Assert
         Assert.AreEqual(existedSubscriber,findedSubscriber);
