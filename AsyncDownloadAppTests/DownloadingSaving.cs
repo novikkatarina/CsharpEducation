9c66a1da05e972dccb164384c60c785970d19e8b
Katarina Novikova <novikkatarina@gmail.com>
2023-08-28 19:37:30 +0400

    Complited

---
+++ AsyncDownloadAppTests/DownloadingSaving.cs
@@ -0,0 +1,40 @@
+using AsyncDownloadApp;
+
+namespace AsyncDownloadAppTests;
+
+public class Tests
+{
+  public DownloaderSaver ds { set; get; }
+
+  [SetUp]
+  public void Setup()
+  {
+    ds = new DownloaderSaver();
+  }
+
+  [Test]
+  public async Task DownloadingSaving_File_SuccessfullyDownloadedAndSaved()
+  {
+    // Arrange
+    string path = "https://youtu.be/GSItB6R7V2g";
+    string name = "/Users/user/Downloads/12354.mp4";
+
+    var path2 =
+      "https://okeygeek.ru/wp-content/uploads/2019/01/%D0%A1%D0%BD%D0%B8%D0%BC%D0%BE%D0%BA-%D1%8D%D0%BA%D1%80%D0%B0%D0%BD%D0%B0-2019-01-31-%D0%B2-0.14.25.png";
+    var name2 = "/Users/user/Downloads/1234.png";
+
+    // Act
+    var tasks = new List<Task<DownloaderSaver.ExecutingAppTimeCharacteristics>>
+    {
+      ds.AsyncDownloadSave(path, name), ds.AsyncDownloadSave(path2, name2)
+    };
+
+    await Task.WhenAll(tasks);
+
+    var result1 = tasks[0].Result;
+    var result2 = tasks[1].Result;
+
+    //Assert
+    Assert.IsTrue(result1.TimeOfFinishing > result2.StartingTime);
+  }
+}
