aec1f992a2b57105f5f97ca11fb51961b3c7bf0f
Katarina Novikova <novikkatarina@gmail.com>
2023-08-31 18:44:20 +0400

    Fixed

--- Phonebook-master/Phonebook.Tests/PhoneNumberValidatorTests.cs
+++ Phonebook-master/Phonebook.Tests/PhoneNumberValidatorTests.cs
@@ -7,13 +7,6 @@ public class PhoneNumberValidatorTests
     [Test]
     public void ValidateNumber_InvalidNumber_ThrowsException()
     {
-        // try
-        // {
-        // PhoneNumberValidator.Validate(new PhoneNumber("123", PhoneNumberType.Personal));
-        //     Assert.Fail();
-        // }
-        // catch (ArgumentException)
-        // {
         Assert.Throws<ArgumentException>(() =>
             PhoneNumberValidator.Validate(new PhoneNumber("+123 789-0123", PhoneNumberType.Personal)));
     }
